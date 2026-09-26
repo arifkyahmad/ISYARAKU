07b336dd94ccd16291abe7ea5faf0f7f81229f90
arifkyahmad <arifkyahmad346@gmail.com>
2026-09-26 19:09:02 +0700

    fix bug toggle password, tambah id, integrasi Supabase auth, fix konflik nama variabel supabase

--- frontend/js/supabaseClient.js
+++ frontend/js/supabaseClient.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   "https://ogozgbkfgdlzlfxbmrff.supabase.co",
   "sb_publishable_p4KbttxSFHwJ0MWJf-ZYzw_us_3Hgd9",
 );
